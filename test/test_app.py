bc7c075cbed0a47c9c4140aa30f7739458b709cf
Gulshan Kumar <152801380+gulshankumar586@users.noreply.github.com>
2026-08-12 23:12:50 +0530

    Update test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -2,5 +2,5 @@ from Backend.app import app
 
 def test_home_route():
     client = app.test_client()
-    response = client.get('/health')
+    response = client.get('/')
     assert response.status_code == 200
